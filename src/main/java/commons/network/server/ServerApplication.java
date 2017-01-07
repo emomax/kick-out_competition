426bb53979affa2118448f5671a9fd3166fad6c5
Max Teleman <max.teleman@gmail.com>
2017-01-08 00:55:06 +0100

    Added support for controllers outside main boot package

--- src/main/java/commons/network/server/ServerApplication.java
+++ src/main/java/commons/network/server/ServerApplication.java
@@ -2,8 +2,12 @@ package commons.network.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import spacerace.server.SpaceRaceRestController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = SpaceRaceRestController.class)
 public class ServerApplication {
 
     public static void main(final String[] args) {
