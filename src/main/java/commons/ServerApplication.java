06e2dcb87c7038fd93ca325fc13bc0f5ac925694
Max Teleman <max.teleman@gmail.com>
2017-01-19 20:32:45 +0100

    Moved main server application, the SpringBootApplication, from network package to common

--- src/main/java/commons/ServerApplication.java
+++ src/main/java/commons/ServerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import commons.network.server.ServerRestController;
 import spacerace.server.communication.rest.SpaceRaceRestController;
 import spacerace.server.communication.socket.SpaceRaceSocketServer;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = SpaceRaceRestController.class)
 @ComponentScan(basePackageClasses = SpaceRaceSocketServer.class)
+@ComponentScan(basePackageClasses = ServerRestController.class)
 public class ServerApplication {
 
     public static void main(final String[] args) {
