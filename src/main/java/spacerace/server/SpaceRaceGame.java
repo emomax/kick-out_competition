c77d58e2ea47a6d935d82b5299bb31a4298306a4
Max Teleman <max.teleman@gmail.com>
2017-02-17 15:05:55 +0100

    Upped the join time to 30s so that everyone has time to join

--- src/main/java/spacerace/server/SpaceRaceGame.java
+++ src/main/java/spacerace/server/SpaceRaceGame.java
@@ -32,7 +32,7 @@ import static java.util.Arrays.asList;
 
 public class SpaceRaceGame {
 
-    public static final  int         JOIN_GAME_TIMEOUT    = 15;
+    public static final  int         JOIN_GAME_TIMEOUT    = 30;
     private static final List<Color> STANDARD_SHIP_COLORS = asList(RED, BLUE, MAGENTA, GREEN, CYAN, WHITE, YELLOW, PINK);
 
     private final UUID   id;
